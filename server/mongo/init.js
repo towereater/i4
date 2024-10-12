9ca8ca82fecac2ffc9e51e0222e2774cc14a5688
Andrea Nicoletti <andrea.n96@hotmail.it>
2024-10-13 00:53:59 +0200

    Created init script for mongo docker image

--- server/mongo/init.js
+++ server/mongo/init.js
@@ -1,10 +1,10 @@
 var res
 
+use("i4")
+
 db.createCollection("metadata")
-metadata = db.collection("metadata")
-res = await metadata.createIndex({ hash: 1 })
-res = await metadata.createIndex({ client: 1, machine: 1, timestamp: 1 })
+res = db.metadata.createIndex({ hash: 1 }, { name: "hash-idx" })
+res = db.metadata.createIndex({ client: 1, machine: 1, timestamp: 1 }, { name: "client-idx" })
 
 db.createCollection("content")
-content = db.collection("content")
-res = await content.createIndex({ hash: 1 })
+res = db.content.createIndex({ hash: 1 }, { name: "hash-idx" })
