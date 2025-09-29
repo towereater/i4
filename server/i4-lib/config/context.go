cff85c1b6e3ca731b3dc9cdb0780cf148aa5bd53
Andrea Nicoletti <andrea.n96@hotmail.it>
2025-09-29 12:43:10 +0200

    Added new query parameters

--- server/i4-lib/config/context.go
+++ server/i4-lib/config/context.go
@@ -8,5 +8,15 @@ const ContextHash ContextKey = "hash"
 const ContextClientCode ContextKey = "client"
 const ContextMachineCode ContextKey = "machine"
 
+const ContextDataType ContextKey = "type"
+const ContextOperation ContextKey = "op"
+const ContextTimestampFrom ContextKey = "tsfrom"
+const ContextTimestampTo ContextKey = "tsto"
+const ContextDataKey ContextKey = "key"
+const ContextDataValue ContextKey = "value"
+
+const ContextFrom ContextKey = "from"
+const ContextLimit ContextKey = "limit"
+
 // Constants
 const ClientAdminCode string = "00000"
