66824ad36f931c296e1ede697c23b9605cc12b64
Andrea Nicoletti <andrea.n96@hotmail.it>
2025-09-29 12:42:54 +0200

    Added select functions for data

--- server/i4-lib/db/gauge.go
+++ server/i4-lib/db/gauge.go
@@ -3,7 +3,64 @@ package db
 import (
 	"i4-lib/config"
 	"i4-lib/model"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func SelectGauge(cfg config.DBConfig, client string, dataFilter model.DataGauge, tsFrom string, tsTo string, limit int) ([]model.DataGauge, error) {
+	// Setup timeout
+	ctx, cancel := getContextFromConfig(cfg)
+	defer cancel()
+
+	// Retrieve the collection
+	coll, err := getClientCollection(ctx, cfg, client, cfg.Collections.Gauge)
+	if err != nil {
+		return []model.DataGauge{}, err
+	}
+
+	// Setup find options
+	var opts options.FindOptions
+	opts.SetLimit(int64(limit))
+
+	// Setup filter
+	filter := bson.M{}
+	if dataFilter.Machine != "" {
+		filter["machine"] = dataFilter.Machine
+	}
+	if dataFilter.Key != "" {
+		filter["key"] = dataFilter.Key
+	}
+	if dataFilter.Value != "" {
+		filter["value"] = dataFilter.Value
+	}
+
+	if dataFilter.Timestamp != "" {
+		filter["ts"] = dataFilter.Timestamp
+	} else {
+		tsFilter := bson.M{}
+		if tsFrom != "" {
+			tsFilter["gt"] = tsFrom
+			filter["ts"] = tsFilter
+		}
+		if tsTo != "" {
+			tsFilter["lt"] = tsTo
+			filter["ts"] = tsFilter
+		}
+	}
+
+	// Define the cursor
+	cursor, err := coll.Find(ctx, filter, &opts)
+	if err != nil {
+		return []model.DataGauge{}, err
+	}
+
+	// Search for the documents
+	var data []model.DataGauge
+	err = cursor.All(ctx, &data)
+
+	return data, err
+}
 
 func InsertGauge(cfg config.DBConfig, client string, data model.DataGauge) error {
 	// Setup timeout
--- server/i4-lib/db/interval.go
+++ server/i4-lib/db/interval.go
@@ -3,7 +3,62 @@ package db
 import (
 	"i4-lib/config"
 	"i4-lib/model"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func SelectInterval(cfg config.DBConfig, client string, dataFilter model.DataInterval, tsFrom string, tsTo string, limit int) ([]model.DataInterval, error) {
+	// Setup timeout
+	ctx, cancel := getContextFromConfig(cfg)
+	defer cancel()
+
+	// Retrieve the collection
+	coll, err := getClientCollection(ctx, cfg, client, cfg.Collections.Interval)
+	if err != nil {
+		return []model.DataInterval{}, err
+	}
+
+	// Setup find options
+	var opts options.FindOptions
+	opts.SetLimit(int64(limit))
+
+	// Setup filter
+	filter := bson.M{}
+	if dataFilter.Machine != "" {
+		filter["machine"] = dataFilter.Machine
+	}
+	if dataFilter.Key != "" {
+		filter["key"] = dataFilter.Key
+	}
+	if dataFilter.Value != "" {
+		filter["value"] = dataFilter.Value
+	}
+
+	tsFilter := bson.M{}
+	if tsFrom != "" {
+		tsFilter["gt"] = tsFrom
+		filter["start"] = tsFilter
+		filter["end"] = tsFilter
+	}
+	if tsTo != "" {
+		tsFilter["lt"] = tsTo
+		filter["start"] = tsFilter
+		filter["end"] = tsFilter
+	}
+
+	// Define the cursor
+	cursor, err := coll.Find(ctx, filter, &opts)
+	if err != nil {
+		return []model.DataInterval{}, err
+	}
+
+	// Search for the documents
+	var data []model.DataInterval
+	err = cursor.All(ctx, &data)
+
+	return data, err
+}
 
 func InsertInterval(cfg config.DBConfig, client string, data model.DataInterval) error {
 	// Setup timeout
